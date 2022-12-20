6f5b01887fa5fcebfd5630ed64d3cbcb0df2d43b
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-20 23:22:45 +0100

    Start of project

---
+++ main.py
@@ -0,0 +1,33 @@
+# google text to speech
+"""import gtts
+from playsound import playsound
+
+tts = gtts.gTTS("AHoj šmejde", lang="cs")
+tts.save("hello.mp3")
+playsound("hello.mp3")
+"""
+# import pyttsx3 as pyttsx3
+
+# python text to speech
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("HELLO")
+engine.runAndWait()
+
+
+engine = pyttsx3.init("dummy")
+voices = engine.getProperty('voices')
+for voice in voices:
+    print(f"Voice: {voice.name}")
+
+
+"""engine = pyttsx3.init()
+text = "Python is a great programming language"
+engine.say(text)
+engine.say("Hi")
+# play the speech
+engine.runAndWait()
+rate = engine.getProperty("rate")
+print(rate)
+voices = engine.getProperty("voices")
+print(voices)"""
