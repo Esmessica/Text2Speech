af324dd7494283b1bc0ef697ba8d483a9b6d2c11
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-26 23:49:34 +0100

    PDF to mp3 without OOP

--- PDF_do_mp3.py
+++ PDF_do_mp3.py
@@ -2,11 +2,24 @@ import PyPDF2
 import gtts
 from playsound import playsound
 
+print("\tEnter name of your desired file below.\n(Please remember that file has to be in same"
+      " folder with this program OR enter path to file-example below)\n")
+print("Example of path: 'C:/Users/my_name/Desktop/NAME_OF_PDF_FILE'\n")
 
-path = open("spalovac_mrtvol.pdf", "rb")
+pdf_file = input("PDF: ")
+
+path = open(f"{pdf_file}.pdf", "rb")
+
+
 reader = PyPDF2.PdfReader(path)
-frompage = reader.pages[144]
+frompage = reader.pages[1]
 text = frompage.extract_text()
 tts = gtts.gTTS(f"{text}", lang="cs")
-tts.save("hello.mp3")
-playsound("hello.mp3")
+file_name = f"{pdf_file}"
+
+if "_" in pdf_file:
+      f = file_name.replace("_", "")
+      tts.save(f"{f}.mp3")
+else:
+      tts.save(f"{pdf_file}.mp3")
+    #  playsound(f"{pdf_file}.mp3")
