c21347481bc2c1e40227b8afaa2a9f32ac867ef0
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-27 22:09:04 +0100

    PDF_do_mp3.py - adding page counter of whole document

--- PDF_do_mp3.py
+++ PDF_do_mp3.py
@@ -5,13 +5,24 @@ import gtts
 
 print("\tEnter name of your desired file below.\n(Please remember that file has to be in same"
       " folder with this program OR enter path to file-example below)\n")
-print("Example of path: '/Users/my_name/Desktop/NAME_OF_PDF_FILE'\n")
+print("Example of path: 'C:/Users/my_name/Desktop/NAME_OF_PDF_FILE'\n")
 
 try:
-
     pdf_file = input("PDF: ")
     path = open(f"{pdf_file}.pdf", "rb")
     reader = PyPDF2.PdfReader(path)
+
+    pocet = 0
+    for strana in path:        # counts pages
+        pocet = pocet + 1
+
+    print(f"{pocet} pages long document")
+
+    if 5 < pocet < 50 :
+        print("This file is abit long, might take a moment")
+    elif pocet > 50:
+        print("This might take some time, make yourself comfy")
+
     frompage = reader.pages[2]
     text = frompage.extract_text()
     tts = gtts.gTTS(f"{text}", lang="cs")
@@ -23,7 +34,6 @@ try:
 
     else:                   # code that runs if _ is not in file name
         tts.save(f"{pdf_file}.mp3")
-        #  playsound(f"{pdf_file}.mp3")
 
     print("\nDone")
 
