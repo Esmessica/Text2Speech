5b2ccc17ba0e1f1cc77144b94d3c2d7976c60f17
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-27 23:37:25 +0100

    test_sub.py - testing whole pdf to mp3 file v2 -adeed time counter

--- test_sub.py
+++ test_sub.py
@@ -1,20 +1,23 @@
 import PyPDF2
 import pyttsx3
 import pdfplumber
+import time
 from playsound import playsound
-
+now = time.time()
 file = "romeo.pdf"
 book = open(file, "rb")
 pdf_reader = PyPDF2.PdfReader(book)
 pages = len(pdf_reader.pages)
 final_text = ""
 with pdfplumber.open(file) as pdf:
-    for i in range(0, 50):       # pages
+    for i in range(0, 20):       # pages
         page = pdf.pages[i]
         text = page.extract_text()
         final_text += text
 engine = pyttsx3.init()
 engine.save_to_file(final_text, "test.mp3")
 engine.runAndWait()
+minus = time.time()
 
 print("DONE")
+print(f"Took {int(minus - now)} seconds to finnish")
