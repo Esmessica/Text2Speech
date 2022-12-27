bb538f526439c0422c671ca9ab85b51de74f3f9e
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-27 23:29:41 +0100

    test_sub.py - testing whole pdf to mp3 file v2

--- test_sub.py
+++ test_sub.py
@@ -1,19 +1,20 @@
 import PyPDF2
-import gtts
+import pyttsx3
 import pdfplumber
 from playsound import playsound
 
-file = "spalovac_mrtvol.pdf"
-
+file = "romeo.pdf"
 book = open(file, "rb")
 pdf_reader = PyPDF2.PdfReader(book)
 pages = len(pdf_reader.pages)
 final_text = ""
 with pdfplumber.open(file) as pdf:
-    for i in range(0, pages):
+    for i in range(0, 50):       # pages
         page = pdf.pages[i]
         text = page.extract_text()
         final_text += text
-tts = gtts.gTTS(f"{final_text}", lang="cs")
-tts.save("hello.mp3")
-playsound("hello.mp3")
+engine = pyttsx3.init()
+engine.save_to_file(final_text, "test.mp3")
+engine.runAndWait()
+
+print("DONE")
