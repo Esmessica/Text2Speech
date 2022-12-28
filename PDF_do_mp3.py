724b2a0fb9ac487b61233b8879ac8b8271fdd828
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-29 00:09:51 +0100

    PDF_do_mp3.py testing errors

--- PDF_do_mp3.py
+++ PDF_do_mp3.py
@@ -9,20 +9,22 @@ print("\tEnter name of your desired file below.\n(Please remember that file has 
 print("Example of path: 'C:/Users/my_name/Desktop/NAME_OF_PDF_FILE'\n")
 
 try:
-    pdf_file = input("PDF: ")
-    path = open(f"{pdf_file}.pdf", "rb")
+    pdf_name = input("PDF: ")
+    file = f"{pdf_name}"
+
+    path = open(f"{file}.pdf", "rb")
     reader = PyPDF2.PdfReader(path)
     pages = len(reader.pages)
 
     final_text = ""
 
-    with pdfplumber.open(pdf_file) as pdf:
+    with pdfplumber.open(f"{file}.pdf") as pdf:
         for i in range(0, pages):
             page = pdf.pages[i]
             text = page.extract_text()
             final_text += text
 
-    num_pages = 0
+    """num_pages = 0
     for page in path:            # counts pages
         num_pages = num_pages + 1
         frompage = len(reader.pages)
@@ -32,9 +34,9 @@ try:
         print("This file is abit long, might take a moment")
     elif num_pages > 50:
         print("This might take some time, make yourself comfy")  # prints sentence based on lenght of pdf
-
-    text = frompage.extract_text()
-    tts = gtts.gTTS(f"{text}", lang="cs")
+    """
+    """    #  text = frompage.extract_text()           # library for saving file start of code
+    tts = gtts.gTTS(f"{final_text}", lang="cs")       # tts = gtts.gTTS(f"{text}", lang="cs")
     file_name = f"{pdf_file}"
 
     if "_" in pdf_file:     # decision for shortening final mp3 file
@@ -42,7 +44,11 @@ try:
         tts.save(f"{f}.mp3")
 
     else:                   # code that runs if _ is not in file name
-        tts.save(f"{pdf_file}.mp3")
+        tts.save(f"{pdf_file}.mp3")"""
+
+    engine = pyttsx3.init()
+    engine.save_to_file(final_text, f"{pdf_name}.mp3")
+    engine.runAndWait()
 
     print("\nDone")
 
